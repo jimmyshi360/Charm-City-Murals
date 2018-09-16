d891395572e45a4dae3e721b14586a446ff93769
Jimmy Shi <relarix@Jimmys-MacBook-Pro.local>
2018-09-15 21:48:17 -0400

    new updated website

--- main/static/app.js
+++ main/static/app.js
@@ -25,7 +25,7 @@ particlesJS("particles-js", {
       "image": {
         "src": "img/github.svg",
         "width": 100,
-        "height": 100
+        "height": 150
       }
     },
     "opacity": {
